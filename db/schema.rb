34f7559d207cca522225eb967dc011e7353c7332
Eric Loudon <loudon.eric@gmail.com>
2019-10-08 10:04:52 +0000

    Done.

--- db/schema.rb
+++ db/schema.rb
@@ -11,6 +11,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 20160310233656) do
 
-end
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.string "password"
+  end
